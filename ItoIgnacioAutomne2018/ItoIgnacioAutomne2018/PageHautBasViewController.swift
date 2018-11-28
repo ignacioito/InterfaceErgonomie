de05cb401f1a1b28dd14381b332e2d9240bd6c03
Ignacio <ignacioito@gmail.com>
2018-11-27 21:29:46 -0500

    commit

---
+++ ItoIgnacioAutomne2018/ItoIgnacioAutomne2018/PageHautBasViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageHautBasViewController.swift
+//  ItoIgnacioAutomne2018
+//
+//  Created by eleves on 18-11-27.
+//  Copyright © 2018 eleves. All rights reserved.
+//
+
+import UIKit
+
+class PageHautBasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

598aefb0438df73534bf12f72ccf489bb0d26cc5
Ignacio <ignacioito@gmail.com>
2018-12-04 20:12:36 -0500

    commit

--- ItoIgnacioAutomne2018/ItoIgnacioAutomne2018/PageHautBasViewController.swift
+++ ItoIgnacioAutomne2018/ItoIgnacioAutomne2018/PageHautBasViewController.swift
@@ -9,14 +9,48 @@
 import UIKit
 
 class PageHautBasViewController: UIViewController {
-
+    @IBOutlet weak var lb01: UILabel!
+    
+    @IBOutlet weak var tx01: UITextField!
+    
+    @IBOutlet weak var lb02: UILabel!
+    
+    @IBOutlet weak var tx02: UITextField!
+    
+    @IBOutlet weak var lb03: UILabel!
+    
+    @IBOutlet weak var tx03: UITextField!
+    
+    @IBOutlet weak var lb04: UILabel!
+    
+    @IBOutlet weak var tx04: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        resetTextFields()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dissmissKeyboard))
+        
+        self.view.addGestureRecognizer(tapGesture)
+        
     }
     
-
+    func resetTextFields() {
+        tx01.text = "0"
+        tx01.delegate = self
+        tx02.text = "0"
+        tx02.delegate = self
+        tx03.text = "0"
+        tx03.delegate = self
+        tx04.text = "0"
+        tx04.delegate = self
+        
+    }
+    
+    @objc func dissmissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,5 +60,16 @@ class PageHautBasViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func validation(_ sender: Any) {
+    }
+    
+}
+extension PageHautBasViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
 }
